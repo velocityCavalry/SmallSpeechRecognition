fb358a5dffbaeb813c4d830f662994e8aa9f2279
velocityCalvary <xyu530@uw.edu>
2019-10-19 16:53:28 -0700

    Initial Commit

---
+++ haha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  haha
+//
+//  Created by 余忻妍 on 10/19/19.
+//  Copyright © 2019 velocity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
